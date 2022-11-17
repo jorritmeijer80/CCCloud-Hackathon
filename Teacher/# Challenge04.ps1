4a40615c06199b938f14c350c67fbadfbfa73511
Jorrit Meijer <55590424+jorritmeijer80@users.noreply.github.com>
2022-11-17 17:42:28 +0100

    Delete Teacher directory

--- Teacher/# Challenge04.ps1
+++
@@ -1,6 +0,0 @@
-# Deploy a web app from a geo-replicated Azure container registry
-
-Pull images from an Azure container registry to various deployment targets:
-
-Scalable orchestration systems that manage containerized applications across clusters of hosts, including Kubernetes, DC/OS, and Docker Swarm.
-Azure services that support building and running applications at scale, including Azure Kubernetes Service (AKS), App Service, Azure Container Instances, Azure Container Apps, Batch, Service Fabric, and others.
